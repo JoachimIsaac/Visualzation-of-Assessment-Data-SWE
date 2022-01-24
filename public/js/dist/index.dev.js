6157dbd33490b19fb7c7536d86539c2f34552b2d
Joachim Isaac <joachimvivian3@gmail.com>
2022-01-24 12:50:29 -0600

    testing

--- public/js/dist/index.dev.js
+++ public/js/dist/index.dev.js
@@ -4,7 +4,7 @@
 //     $('#head1').hide();
 // });
 var hello = function hello() {
-  return "Hi welcome to my starter kit";
+  return "Hi welcome to my starter kit - frontend";
 };
 
 console.log(hello());
